947c6d33f7b6ac98d1e6f4fb097eb7d753d89c25
V. Kutsarit <kutsarit@web.de>
2019-02-16 15:18:51 +0100

    translate CartesianKinematics step pulse generator

---
+++ sharp/KlipperSharp/CartesianKinematicsSptg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlipperSharp
+{
+	// Cartesian kinematics stepper pulse time generation
+	public class CartesianKinematicsSptg
+	{
+
+		static double cart_stepper_x_calc_position(ref stepper_kinematics sk, ref move m, double move_time)
+		{
+			return Itersolve.move_get_coord(ref m, move_time).x;
+		}
+
+		static double cart_stepper_y_calc_position(ref stepper_kinematics sk, ref move m, double move_time)
+		{
+			return Itersolve.move_get_coord(ref m, move_time).y;
+		}
+
+		static double cart_stepper_z_calc_position(ref stepper_kinematics sk, ref move m, double move_time)
+		{
+			return Itersolve.move_get_coord(ref m, move_time).z;
+		}
+
+		public static stepper_kinematics cartesian_stepper_alloc(char axis)
+		{
+			stepper_kinematics sk = new stepper_kinematics();
+			if (axis == 'x')
+				sk.calc_position = cart_stepper_x_calc_position;
+			else if (axis == 'y')
+				sk.calc_position = cart_stepper_y_calc_position;
+			else if (axis == 'z')
+				sk.calc_position = cart_stepper_z_calc_position;
+			return sk;
+		}
+
+	}
+}
