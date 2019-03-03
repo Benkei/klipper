1b570df4d5979e950e43290315ecef7873c37bdd
Kutsarit V <Kutsarit@web.de>
2019-03-03 17:17:43 +0100

    refactor split code

---
+++ sharp/KlipperSharp/IO/MessageParser.MessageFormat.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace KlipperSharp
+{
+	public abstract class BaseFormat
+	{
+		public string Name;
+		public int Msgid;
+		public string Msgformat;
+		public string Debugformat;
+		public List<PT_Type> Param_types;
+		public List<(string Name, PT_Type Type)> Param_names;
+
+		public virtual void Encode(object[] parameters, BinaryWriter writer)
+		{
+			writer.Write((byte)Msgid);
+			for (int i = 0; i < Param_types.Count; i++)
+			{
+				Param_types[i].encode(writer, parameters[i]);
+			}
+		}
+		public virtual void Encode_by_name(Dictionary<string, object> parameters, BinaryWriter writer)
+		{
+			writer.Write((byte)Msgid);
+			for (int i = 0; i < Param_names.Count; i++)
+			{
+				Param_names[i].Type.encode(writer, parameters[Param_names[i].Name]);
+			}
+		}
+		public abstract Dictionary<string, object> Parse(ReadOnlySpan<byte> s, ref int pos);
+		public abstract string Format_params(Dictionary<string, string> parameters);
+	}
+	public class MessageFormat : BaseFormat
+	{
+		public Dictionary<string, PT_Type> Name_to_type;
+
+		public MessageFormat(int msgid, string msgformat)
+		{
+			Msgid = msgid;
+			Msgformat = msgformat;
+			Debugformat = MessageParser.Convert_msg_format(msgformat);
+			Param_types = new List<PT_Type>();
+			Param_names = new List<(string, PT_Type)>();
+			Name_to_type = new Dictionary<string, PT_Type>();
+
+			var match = MessageParser.MessageFormatRegex.Match(msgformat);
+			while (match.Success)
+			{
+				var cmd = match.Groups["CMD"];
+				var param = match.Groups["PARAM"];
+				var value = match.Groups["VALUE"];
+				if (cmd.Success)
+				{
+					Name = cmd.Value;
+				}
+				if (param.Success && value.Success)
+				{
+					var handle = MessageParser.MessageTypes[value.Value];
+					Param_types.Add(handle);
+					Param_names.Add((param.Value, handle));
+					Name_to_type.Add(param.Value, handle);
+				}
+				match = match.NextMatch();
+			}
+		}
+
+		public override Dictionary<string, object> Parse(ReadOnlySpan<byte> s, ref int pos)
+		{
+			pos++;
+			var output = new Dictionary<string, object>(Param_names.Count);
+			for (int i = 0; i < Param_names.Count; i++)
+			{
+				output[Param_names[i].Name] = Param_names[i].Type.parse(s, ref pos);
+			}
+			return output;
+		}
+		public override string Format_params(Dictionary<string, string> parameters)
+		{
+			var result = new List<object>(1 + Param_names.Count);
+			for (int i = 0; i < Param_names.Count; i++)
+			{
+				var v = parameters[Param_names[i].Name];
+				if (!Param_names[i].Type.is_integer)
+				{
+					// v = repr(v)
+				}
+				result.Add(v);
+			}
+			return string.Format(Debugformat, result.ToArray());
+		}
+	}
+	public class OutputFormat : BaseFormat
+	{
+		public OutputFormat(int msgid, string msgformat)
+		{
+			Name = "#output";
+			Msgid = msgid;
+			Msgformat = msgformat;
+			Debugformat = MessageParser.Convert_msg_format(msgformat);
+			Param_types = new List<PT_Type>();
+
+			var match = MessageParser.MessageFormatRegex.Match(msgformat);
+			while (match.Success)
+			{
+				var cmd = match.Groups["CMD"];
+				var param = match.Groups["PARAM"];
+				var value = match.Groups["VALUE"];
+				if (cmd.Success)
+				{
+					Name = cmd.Value;
+				}
+				if (param.Success && value.Success)
+				{
+					var handle = MessageParser.MessageTypes[value.Value];
+					Param_types.Add(handle);
+				}
+				match = match.NextMatch();
+			}
+		}
+
+		public override Dictionary<string, object> Parse(ReadOnlySpan<byte> s, ref int pos)
+		{
+			pos++;
+			var output = new string[Param_types.Count];
+			for (int i = 0; i < Param_types.Count; i++)
+			{
+				var data = Param_types[i].parse(s, ref pos);
+				if (data == null)
+					data = "null";
+				output[i] = data as string;
+			}
+			var outmsg = string.Format(Debugformat, output);
+			var dict = new Dictionary<string, object>(1) {
+				{ "#msg", outmsg }
+			};
+			return dict;
+		}
+		public override string Format_params(Dictionary<string, string> parameters)
+		{
+			return $"#output {parameters["#msg"]}";
+		}
+	}
+	public class UnknownFormat : BaseFormat
+	{
+		public UnknownFormat()
+		{
+			Name = "#unknown";
+		}
+		public unsafe override Dictionary<string, object> Parse(ReadOnlySpan<byte> s, ref int pos)
+		{
+			var msgid = s[pos];
+
+			byte* block = stackalloc byte[s.Length];
+			s.CopyTo(new Span<byte>(block, s.Length));
+			var msg = Encoding.ASCII.GetString(block, s.Length);
+
+			pos += s.Length - MessageParser.MESSAGE_TRAILER_SIZE;
+			var output = new Dictionary<string, object>(2) {
+				{ "#msgid", msgid },
+				{ "#msg", msg }
+			};
+			return output;
+		}
+		public override string Format_params(Dictionary<string, string> parameters)
+		{
+			return $"#unknown {parameters["#msg"]}";
+		}
+	}
+}
---
+++ sharp/KlipperSharp/IO/MessageParser.ParameterType.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace KlipperSharp
+{
+	public abstract class PT_Type
+	{
+		public bool is_integer = false;
+		public int max_length = 5;
+		public bool signed = false;
+
+		public abstract void encode(BinaryWriter output, object value);
+		public abstract object parse(ReadOnlySpan<byte> text, ref int position);
+	}
+	public class PT_uint32 : PT_Type
+	{
+		public PT_uint32()
+		{
+			is_integer = true;
+		}
+		public override void encode(BinaryWriter output, object value)
+		{
+			var v = Convert.ToInt64(value);
+			if (v >= 0xc000000 || v < -0x4000000) output.Write((byte)((v >> 28) & 0x7f | 0x80));
+			if (v >= 0x180000 || v < -0x80000) output.Write((byte)((v >> 21) & 0x7f | 0x80));
+			if (v >= 0x3000 || v < -0x1000) output.Write((byte)((v >> 14) & 0x7f | 0x80));
+			if (v >= 0x60 || v < -0x20) output.Write((byte)((v >> 7) & 0x7f | 0x80));
+			output.Write((byte)(v & 0x7f));
+		}
+		public override object parse(ReadOnlySpan<byte> data, ref int position)
+		{
+			var c = data[position++];
+			uint v = c & 0x7fu;
+			if ((c & 0x60) == 0x60)
+				v |= unchecked((uint)-0x20);
+			while ((c & 0x80) > 0)
+			{
+				c = data[position++];
+				v = (v << 7) | (c & 0x7fu);
+			}
+			if (signed)
+				return (int)v;
+			else
+				return v;
+		}
+	}
+	public class PT_int32 : PT_uint32
+	{
+		public PT_int32()
+		{
+			signed = true;
+		}
+	}
+	public class PT_uint16 : PT_uint32
+	{
+		public PT_uint16()
+		{
+			max_length = 3;
+		}
+	}
+	public class PT_int16 : PT_int32
+	{
+		public PT_int16()
+		{
+			signed = true;
+			max_length = 3;
+		}
+	}
+	public class PT_byte : PT_uint32
+	{
+		public PT_byte()
+		{
+			max_length = 2;
+		}
+	}
+	public class PT_string : PT_Type
+	{
+		public PT_string()
+		{
+			is_integer = false;
+			max_length = SerialQueue.MESSAGE_MAX;
+		}
+		public override void encode(BinaryWriter output, object value)
+		{
+			output.Write((byte)((string)value).Length);
+			output.Write((string)value);
+		}
+		public unsafe override object parse(ReadOnlySpan<byte> text, ref int position)
+		{
+			var l = text[position];
+			byte* block = stackalloc byte[l];
+			text[Range.Create(position + 1, position + 1 + l)].CopyTo(new Span<byte>(block, l));
+			var txt = Encoding.ASCII.GetString(block, l);
+			position += l + 1;
+			return txt;
+		}
+	}
+	public class PT_progmem_buffer : PT_buffer
+	{ }
+	public class PT_buffer : PT_Type
+	{
+		public PT_buffer()
+		{
+			is_integer = false;
+			max_length = SerialQueue.MESSAGE_MAX;
+		}
+		public override void encode(BinaryWriter output, object value)
+		{
+			output.Write((byte)((byte[])value).Length);
+			output.Write((byte[])value);
+		}
+		public override object parse(ReadOnlySpan<byte> text, ref int position)
+		{
+			var l = text[position];
+			byte[] block = new byte[l];
+			text[Range.Create(position + 1, position + 1 + l)].CopyTo(new Span<byte>(block));
+			position += l + 1;
+			return block;
+		}
+	}
+}
